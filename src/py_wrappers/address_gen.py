23972ae0f9743c41b6c652812fc5cde5d0f4cbfb
jaxlotl <jacklyn@boringcompany.com>
2022-02-11 16:55:11 -0800

    added simple cli to test wrapper functions for private key, public key, and address generation. invoke by running python3 address_gen.py

--- src/py_wrappers/address_gen.py
+++ src/py_wrappers/address_gen.py
@@ -2,6 +2,8 @@ from ctypes import *
 import sys
 import os
 
+
+
 # LOAD SHARED LIBRARY - there has to be a better way to do this
 dogepath = os.path.abspath(__file__+"../../../../.libs")
 sys.path.insert(0, dogepath)
@@ -9,6 +11,8 @@ dogepath = os.path.join(dogepath, "libdogecoin.so")
 
 cdll.LoadLibrary(dogepath)
 libdoge = CDLL(dogepath)
+
+
 
 # MIMIC C STRUCTS
 class Dogecoin_chain(Structure):
@@ -22,41 +26,134 @@ class Dogecoin_chain(Structure):
         ("netmagic",                    c_ubyte * 4),
     ]
 
-# CALL C FUNCTIONS FROM SHARED LIBRARY
-def py_gen_privkey():
 
-    #init constants from chain.h
+
+# HELPER METHODS
+def get_chain(code=0):
     byte_buf = (c_ubyte * 4)()
-    byte_buf[:] = [0xc0, 0xc0, 0xc0, 0xc0]
-    main = Dogecoin_chain(bytes("main", 'utf-8'), 0x1E, 0x16, 0x9E, 0x02fac398, 0x02facafd, byte_buf)
-    byte_buf[:] = [0xfc, 0xc1, 0xb7, 0xdc]
-    test = Dogecoin_chain(bytes("testnet3", 'utf-8'), 0x71, 0xc4, 0xf1, 0x04358394, 0x043587cf, byte_buf)
-    byte_buf[:] = [0xfa, 0xbf, 0xb5, 0xda]
-    reg = Dogecoin_chain(bytes("regtest", 'utf-8'), 0x6f, 0xc4, 0xEF, 0x04358394, 0x043587CF, byte_buf)
+    
+    # main
+    if code==0:
+        byte_buf[:] = [0xc0, 0xc0, 0xc0, 0xc0]
+        return Dogecoin_chain(bytes("main", 'utf-8'), 0x1E, 0x16, 0x9E, 0x02fac398, 0x02facafd, byte_buf)
+        
+    # testnet
+    elif code==1:           
+        byte_buf[:] = [0xfc, 0xc1, 0xb7, 0xdc]
+        return Dogecoin_chain(bytes("testnet3", 'utf-8'), 0x71, 0xc4, 0xf1, 0x04358394, 0x043587cf, byte_buf)
 
-    #call dogecoin_ecc_start() and gen_privkey()
-    libdoge.dogecoin_ecc_start()
-    libdoge.gen_privatekey.restype = c_bool
+    # regtest
+    elif code==2:
+        byte_buf[:] = [0xfa, 0xbf, 0xb5, 0xda]
+        return Dogecoin_chain(bytes("regtest", 'utf-8'), 0x6f, 0xc4, 0xEF, 0x04358394, 0x043587CF, byte_buf)
+
+    else:
+        print("Bad chain code provided\n")
+
+
+
+# CALL C FUNCTIONS FROM SHARED LIBRARY
+def py_gen_privkey(chain_code):
+
+    #init constants from chain.h... 0=main, 1=testnet, 2=regtest
+    chain = get_chain(chain_code)
+
+    #prepare arguments
     sizeout = 128
     newprivkey_wif = (c_char*sizeout)()
     newprivkey_hex = (c_char*sizeout)()
-    libdoge.gen_privatekey(byref(main), newprivkey_wif, sizeout, newprivkey_hex)
+
+    #call gen_privatekey()
+    libdoge.gen_privatekey.restype = c_bool
+    libdoge.dogecoin_ecc_start()
+    libdoge.gen_privatekey(byref(chain), newprivkey_wif, sizeout, newprivkey_hex)
+    
+    #print result of function
     print("private key wif", str(bytes(newprivkey_wif).decode('utf-8')))
     print("private key hex:", str(bytes(newprivkey_hex).decode('utf-8')))
 
 
+def py_pubkey_from_privatekey(pkey_wif):
+
+    #identify chain - is this method safe?
+    if pkey_wif[0]=='Q':
+        chain = get_chain(0)
+    elif pkey_wif[0]=='c' and pkey_wif[1].isLowercase():
+        chain = get_chain(1)
+    elif pkey_wif[0]=='c' and pkey_wif[1].isUppercase():
+        chain = get_chain(2)
+    else:
+        print(cmd+": private key must be WIF encoded")
+        return
+
+    #prepare arguments
+    pkey = c_char_p(pkey_wif.encode('utf-8'))
+    sz = 128
+    sizeout = c_ulong(sz)
+    pubkey_hex = (c_char * sz)()
+
+    #call pubkey_from_privatekey()
+    libdoge.pubkey_from_privatekey.restype = c_bool
+    libdoge.pubkey_from_privatekey(byref(chain), pkey, byref(pubkey_hex), byref(sizeout))
+    
+    #print result of function
+    print("public key hex:", str(bytes(pubkey_hex).decode('utf-8')))
 
 
-# MAIN METHODS
+def py_address_from_pubkey(chain_code, pubkey_hex):
+    
+    #init constants from chain.h (0=main, 1=testnet, 2=regtest)
+    chain = get_chain(chain_code)
+
+    #prepare arguments
+    pubkey = c_char_p(pubkey_hex.encode('utf-8'))
+    address = (c_char * 40)() # temporary fix, want c_char_p but causes segfault
+
+    #call address_from_pubkey()
+    libdoge.address_from_pubkey.restype = c_bool
+    libdoge.address_from_pubkey(byref(chain), pubkey, byref(address))
+
+    #print result from function
+    print("dogecoin address:", str(bytes(address).decode('utf-8')))
+
+
+
+
+# MAIN METHOD
 if __name__ == "__main__":
+    cmd_lst = ["gen_privkey <which_chain>",
+               "gen_pubkey <privkey_wif>",
+               "gen_address <which_chain> <pubkey_hex>"]
     print("======================================================================")
-    print("Press [q] to quit CLI\n")
+    print("Press [q] to quit CLI")
+    print("Press [w] to repeat previous command\n")
+    print("Available commands:\n")
+    for c in cmd_lst:
+        print(f'\t{c}')
+    print("\n")
+
     inp = input("$ ").split()
     while inp != ['q']:
-        cmd = inp[0]
-        flags = inp[1:]
+        if inp[0]=='w':
+            pass
+        else:
+            cmd = inp[0]
+        args = inp[1:]
         if cmd == "gen_privkey":
-            py_gen_privkey()
+            if args and int(args[0])>=0 and int(args[0])<=2:
+                py_gen_privkey(int(args[0]))
+            else:
+                print(cmd+": enter valid chain code (0:main, 1:test, 2:regtest)")
+        elif cmd == "gen_pubkey":
+            if args and len(args[0]) >= 50:
+                py_pubkey_from_privatekey(args[0])
+            else:
+                print(cmd+": private key must be WIF encoded")
+        elif cmd == "gen_address":
+            if args and int(args[0])>=0 and int(args[0])<=2 and len(args[1])==66:
+                py_address_from_pubkey(int(args[0]), args[1])
+            else:
+                print(cmd+": enter valid chain code (0:main, 1:test, 2:regtest)")
         else:
             print(cmd+": not a valid command")
         print()
