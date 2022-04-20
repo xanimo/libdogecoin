20cb0b132c4c08fb76e6ba6a0da16be077c49d79
Dakoda Greaves <dakoda@xanimo.net>
2022-04-19 18:52:20 -0700

    build shared over static for python3 unit tests

--- depends/packages/libevent.mk
+++ depends/packages/libevent.mk
@@ -5,11 +5,12 @@ $(package)_file_name=libevent-$($(package)_version)-stable.tar.gz
 $(package)_sha256_hash=92e6de1be9ec176428fd2367677e61ceffc2ee1cb119035037a27d346b0403bb
 
 define $(package)_preprocess_cmds
-  ./autogen.sh
+  cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub build-aux
 endef
 
 define $(package)_set_vars
-  $(package)_config_opts=--disable-shared --disable-openssl --disable-libevent-regress
+  $(package)_config_opts=--disable-shared --disable-openssl --disable-libevent-regress --disable-samples
+  $(package)_config_opts += --disable-dependency-tracking --enable-option-checking
   $(package)_config_opts_release=--disable-debug-mode
   $(package)_config_opts_linux=--with-pic
 endef
@@ -27,4 +28,6 @@ define $(package)_stage_cmds
 endef
 
 define $(package)_postprocess_cmds
+  rm lib/*.la && \
+  rm include/ev*.h
 endef
