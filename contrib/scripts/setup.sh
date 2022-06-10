2baa08cb94ddad6d3e4eef255449f7b767d471da
Dakoda Greaves <dakoda@xanimo.net>
2022-06-10 03:38:39 -0700

    contrib: multi-arch testing script

--- contrib/scripts/setup.sh
+++ contrib/scripts/setup.sh
@@ -160,8 +160,12 @@ case "$TARGET_HOST_TRIPLET" in
         contrib/scripts/sdk.sh
     ;;
     *)
+        if [[ $EUID == 0 ]] || [[ $DOCKER == 1 ]]; then
+            dpkg --add-architecture $TARGET_ARCH
+        else
+            echo "please run with sudo"
+        fi
         setup_linux
-        echo $TARGET_ARCH done
     ;;
 esac
 
