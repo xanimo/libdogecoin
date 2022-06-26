bf95d71d729752e1068f53c6fdeb728a3e871d79
Dakoda Greaves <dakoda@xanimo.net>
2022-06-25 20:17:01 -0700

    contrib: signed archiving/.asc/checksum.txt per arch

--- contrib/scripts/package.sh
+++ contrib/scripts/package.sh
@@ -50,39 +50,66 @@ has_param() {
     return 1
 }
 
-HOST=""
-FILES=""
+LIB="`pwd`/.libs/libdogecoin.a `pwd`/include/dogecoin/libdogecoin.h"
+EXE=""
+FILES="$LIB "
 DOCS="`pwd`/doc/*.md README.md"
+PREFIX=""
+BUILD_PATH=""
 if has_param '--host' "$@"; then
     case "$2" in
         "arm-linux-gnueabihf")
-            FILES="`pwd`/.libs/libdogecoin.a `pwd`/include/dogecoin/libdogecoin.h `pwd`/such `pwd`/sendtx"
+            EXE="`pwd`/such `pwd`/sendtx"
+            FILES="$LIB $EXE"
         ;;
         "aarch64-linux-gnu")
-            FILES="`pwd`/.libs/libdogecoin.a `pwd`/include/dogecoin/libdogecoin.h `pwd`/such `pwd`/sendtx"
+            EXE="`pwd`/such `pwd`/sendtx"
+            FILES="$LIB $EXE"
         ;;
         "x86_64-w64-mingw32")
-            FILES="`pwd`/.libs/libdogecoin.a `pwd`/include/dogecoin/libdogecoin.h `pwd`/such.exe `pwd`/sendtx.exe"
+            EXE="`pwd`/such.exe `pwd`/sendtx.exe"
+            FILES="$LIB $EXE"
         ;;
         "i686-w64-mingw32")
-            FILES="`pwd`/.libs/libdogecoin.a `pwd`/include/dogecoin/libdogecoin.h `pwd`/such.exe `pwd`/sendtx.exe"
+            EXE="`pwd`/such.exe `pwd`/sendtx.exe"
+            FILES="$LIB $EXE"
         ;;
         "x86_64-apple-darwin14")
-            FILES="`pwd`/.libs/libdogecoin.a `pwd`/include/dogecoin/libdogecoin.h `pwd`/such `pwd`/sendtx"
+            EXE="`pwd`/such `pwd`/sendtx"
+            FILES="$LIB $EXE"
         ;;
-        "x86_64-pc-linux-gnu") 
-            FILES="`pwd`/.libs/libdogecoin.a `pwd`/include/dogecoin/libdogecoin.h `pwd`/such `pwd`/sendtx"
+        "x86_64-pc-linux-gnu")
+            EXE="`pwd`/such `pwd`/sendtx"
+            FILES="$LIB $EXE"
         ;;
         "i686-pc-linux-gnu")
-            FILES="`pwd`/.libs/libdogecoin.a `pwd`/include/dogecoin/libdogecoin.h `pwd`/such `pwd`/sendtx"
+            EXE="`pwd`/such `pwd`/sendtx"
+            FILES="$LIB $EXE"
         ;;
     esac
+    SUDO="sudo -u $(whoami) -H bash -c"
     if [ "$TAG" != "" ]; then
-        sudo rm -rf ./build/*
-        sudo mkdir -p ./build/libdogecoin-$2-$TAG
-        sudo cp $FILES ./build/libdogecoin-$2-$TAG
-        sha256sum ./build/libdogecoin-$2-$TAG/* > $2-$TAG-checksums.txt
+        PREFIX=`pwd`/build/libdogecoin-$2-$TAG
+        BUILD_PATH=./build/libdogecoin-$2-$TAG
+        # check if target arch build output directory exists and has known file
+        FILE=$BUILD_PATH/libdogecoin.a
+        if test -f "$FILE"; then
+            # if so delete existing files to accomodate clean build
+            $SUDO "rm -rf $BUILD_PATH/*"
+        else
+            # else create directory
+            $SUDO "mkdir -p $BUILD_PATH"
+        fi
+        # copy files to build directory
+        $SUDO "cp -f $FILES $BUILD_PATH"
+        # output sha256 checksums to new text file
+        sha256sum $PREFIX/* >> $2-$TAG-checksums.txt
+        $SUDO "mkdir -p $PREFIX $BUILD_PATH/docs"
+        $SUDO "cp $FILES $BUILD_PATH"
+        $SUDO "cp $DOCS $BUILD_PATH/docs"
+        sha256sum $BUILD_PATH/docs/* >> $2-$TAG-checksums.txt
 
+        # parse repository url to fetch username
         # url="git://github.com/some-user/my-repo.git"
         # url="https://github.com/some-user/my-repo.git"
         url=`git config --get remote.origin.url`
@@ -93,19 +120,42 @@ if has_param '--host' "$@"; then
             protocol=${BASH_REMATCH[1]}
             separator=${BASH_REMATCH[2]}
             hostname=${BASH_REMATCH[3]}
+            # only need user but leaving other vars in case others find useful
             user=${BASH_REMATCH[4]}
             repo=${BASH_REMATCH[5]}
         fi
-        gpg --detach-sign -o $user.sig ./$2-$TAG-checksums.txt
-        gpg --verify $user.sig ./$2-$TAG-checksums.txt
-        sudo mkdir -p ./contrib/sigs/$2-$TAG/$user
-        sudo mv ./$user.sig ./contrib/sigs/$2-$TAG/$user/$user.sig
-        sudo mv ./$2-$TAG-checksums.txt ./contrib/sigs/$2-$TAG/$user/$2-$TAG-checksums.txt
+
+        # sign sha256 checksum file
+        gpg --detach-sign --yes -o $2-$TAG-checksums.txt.sig ./$2-$TAG-checksums.txt
+
+        # verify good signature
+        gpg --verify $2-$TAG-checksums.txt.sig ./$2-$TAG-checksums.txt
+        
+        $SUDO "mkdir -p ./contrib/sigs/$2-$TAG/$user"
+        $SUDO "chmod u=rwx,g=r,o=r ./contrib/sigs/$2-$TAG/$user"
+        $SUDO "mv -u ./$2-$TAG-checksums.txt.sig ./contrib/sigs/$2-$TAG/$user/$2-$TAG-checksums.txt.sig"
+        $SUDO "mv -u ./$2-$TAG-checksums.txt ./contrib/sigs/$2-$TAG/$user/$2-$TAG-checksums.txt"
     else
-        sudo rm -rf ./build/libdogecoin-$2
-        sudo mkdir -p ./build/libdogecoin-$2
-        sudo cp $FILES ./build/libdogecoin-$2
-        sudo mkdir -p ./build/libdogecoin-$2/docs
-        sudo cp $DOCS ./build/libdogecoin-$2/docs
+        PREFIX=`pwd`/build/libdogecoin-$2
+        BUILD_PATH=./build/libdogecoin-$2
+        $SUDO "rm -rf $BUILD_PATH"
+        $SUDO "mkdir -p $BUILD_PATH $BUILD_PATH/docs"
+        $SUDO "cp $FILES $BUILD_PATH"
+        $SUDO "cp $DOCS $BUILD_PATH/docs"
+    fi
+
+    if has_param '--package' "$@"; then
+        if git rev-parse -q --verify "refs/tags/$TAG" >/dev/null; then
+            git tag -d $TAG
+        fi
+        pushd $BUILD_PATH
+            tar -czvf "$2-$TAG.tar.gz" *
+            gpg --armor --detach-sign --yes "$2-$TAG.tar.gz"
+            gpg --verify $2-$TAG.tar.gz.asc $2-$TAG.tar.gz
+            mv $2-$TAG.tar.gz ../$2-$TAG.tar.gz
+            mv $2-$TAG.tar.gz.asc ../$2-$TAG.tar.gz.asc
+        popd
+        rm -rf $BUILD_PATH
+        sha256sum ./build/$2-$TAG.tar.gz ./build/$2-$TAG.tar.gz.asc >> ./contrib/sigs/$2-$TAG/$user/$2-$TAG-checksums.txt
     fi
 fi
