52d03951616b426d1c05d7b7d80473e1e6f51140
bluezr <bluezr@dogecoin.com>
2022-06-26 19:38:26 -0700

    wrappers: update conditional installation of go in build script

--- wrappers/golang/libdogecoin/build.sh
+++ wrappers/golang/libdogecoin/build.sh
@@ -8,9 +8,11 @@ if ! command -v go &> /dev/null
 then
     wget https://go.dev/dl/go1.18.2.linux-amd64.tar.gz
     echo "e54bec97a1a5d230fc2f9ad0880fcbabb5888f30ed9666eca4a91c5a32e86cbc go1.18.2.linux-amd64.tar.gz" | sha256sum -c
-    rm -rf /usr/local/go && tar -C /usr/local -xzf go1.18.2.linux-amd64.tar.gz
-    echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
+    sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.18.2.linux-amd64.tar.gz
+    sudo mv /usr/local/go/bin/go /usr/local/bin/go
+    echo "export PATH=$PATH:/usr/local/bin/go" >> ~/.bashrc
     source ~/.bashrc
+    sudo rm go1.18.2.linux-amd64.tar.gz
     if ! command -v go &> /dev/null
     then
         echo "go could not be found"
