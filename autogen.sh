2c6e4ec6b9ba72cc5c937a3834913fd38e8393fc
Dakoda Greaves <dakoda@xanimo.net>
2022-07-28 16:53:43 -0700

    contrib: backport gitian/guix build system

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
+export LC_ALL=C
 set -e
-autoreconf -if --warnings=all
+srcdir="$(dirname "$0")"
+cd "$srcdir"
+if [ -z "${LIBTOOLIZE}" ] && GLIBTOOLIZE="$(command -v glibtoolize)"; then
+  LIBTOOLIZE="${GLIBTOOLIZE}"
+  export LIBTOOLIZE
+fi
+command -v autoreconf >/dev/null || \
+  (echo "configuration failed, please install autoconf first" && exit 1)
+autoreconf --install --force --warnings=all
+
--- contrib/devtools/split-debug.sh
+++ contrib/devtools/split-debug.sh
@@ -6,5 +6,5 @@ fi
 
 /usr/bin/objcopy --enable-deterministic-archives -p --only-keep-debug $1 $3
 /usr/bin/objcopy --enable-deterministic-archives -p --strip-debug $1 $2
-strip --enable-deterministic-archives -p -s $2
+/usr/bin/strip --enable-deterministic-archives -p -s $2
 /usr/bin/objcopy --enable-deterministic-archives -p --add-gnu-debuglink=$3 $2
