9a5cb6c2a431b074263074710df1202eb2c76f6c
Dakoda Greaves <dakoda@xanimo.net>
2022-12-15 12:20:32 -0800

    save state

--- src/bip39.c
+++ src/bip39.c
@@ -25,12 +25,17 @@
  */
 
 #include <unistd.h>
+#include <uchar.h>
+#include <unistr.h>
+#include <wchar.h>
+#include <uninorm.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <limits.h>
 #include <string.h>
 #include <ctype.h>
+#include <errno.h>
 #include <unicode/utypes.h>
 #include <unicode/ustring.h>
 #include <unicode/unorm2.h>
@@ -250,67 +255,306 @@ int mnemonic_check(const char *mnemonic) {
   return 0;
 }
 
+typedef uint32_t u8chr_t;
+
+// Avoids truncating multibyte UTF-8 encoding at the end.
+char *u8strncpy(char *dest, const char *src, size_t n)
+{
+  int k = n-1;
+  int i;
+  if (n) {
+    dest[k] = 0;
+    strncpy(dest,src,n);
+    if (dest[k] & 0x80) { // Last byte has been overwritten
+      for (i=k; (i>0) && ((k-i) < 3) && ((dest[i] & 0xC0) == 0x80); i--) ;
+      switch(k-i) {
+        case 0:                                 dest[i] = '\0'; break;
+        case 1:  if ( (dest[i] & 0xE0) != 0xC0) dest[i] = '\0'; break;
+        case 2:  if ( (dest[i] & 0xF0) != 0xE0) dest[i] = '\0'; break;
+        case 3:  if ( (dest[i] & 0xF8) != 0xF0) dest[i] = '\0'; break;
+      }
+    }
+  }
+  return dest;
+}
+
+static uint8_t const u8_length[] = {
+// 0 1 2 3 4 5 6 7 8 9 A B C D E F
+   1,1,1,1,1,1,1,1,0,0,0,0,2,2,3,4
+} ;
+
+#define u8length(s) u8_length[(((uint8_t *)(s))[0] & 0xFF) >> 4]
+
+int u8chrisvalid(u8chr_t c)
+{
+  if (c <= 0x7F) return 1;                    // [1]
+
+  if (0xC280 <= c && c <= 0xDFBF)             // [2]
+     return ((c & 0xE0C0) == 0xC080);
+
+  if (0xEDA080 <= c && c <= 0xEDBFBF)         // [3]
+     return 0; // Reject UTF-16 surrogates
+
+  if (0xE0A080 <= c && c <= 0xEFBFBF)         // [4]
+     return ((c & 0xF0C0C0) == 0xE08080);
+
+  if (0xF0908080 <= c && c <= 0xF48FBFBF)     // [5]
+     return ((c & 0xF8C0C0C0) == 0xF0808080);
+
+  return 0;
+}
+
+int u8next(char *txt, u8chr_t *ch)
+{
+   int len;
+   u8chr_t encoding = 0;
+
+   len = u8length(txt);
+  int i=0;
+   for (; i<len && txt[i] != '\0'; i++) {
+      encoding = (encoding << 8) | txt[i];
+   }
+
+   errno = 0;
+   if (len == 0 || !u8chrisvalid(encoding)) {
+      encoding = txt[0];
+      len = 1;
+      errno = EINVAL;
+   }
+
+   if (ch) *ch = encoding;
+
+   return encoding ? len : 0 ;
+}
+
+// from UTF-8 encoding to Unicode Codepoint
+uint32_t u8decode(u8chr_t c)
+{
+  uint32_t mask;
+
+  if (c > 0x7F) {
+    mask = (c <= 0x00EFBFBF)? 0x000F0000 : 0x003F0000 ;
+    c = ((c & 0x07000000) >> 6) |
+        ((c & mask )      >> 4) |
+        ((c & 0x00003F00) >> 2) |
+         (c & 0x0000003F);
+  }
+
+  return c;
+}
+
+// From Unicode Codepoint to UTF-8 encoding
+u8chr_t u8encode(uint32_t codepoint)
+{
+  u8chr_t c = codepoint;
+
+  if (codepoint > 0x7F) {
+    c =  (codepoint & 0x000003F) 
+       | (codepoint & 0x0000FC0) << 2 
+       | (codepoint & 0x003F000) << 4
+       | (codepoint & 0x01C0000) << 6;
+
+    if      (codepoint < 0x0000800) c |= 0x0000C080;
+    else if (codepoint < 0x0010000) c |= 0x00E08080;
+    else                            c |= 0xF0808080;
+  }
+  return c;
+}
+
+int u8chrisblank(u8chr_t c)
+{
+  return (c == 0x20)
+      || (c == 0x09)
+      || (c == 0xC2A0)
+      || (c == 0xE19A80)
+      || ((0xE28080 <= c) && (c <= 0xE2808A))
+      || (c == 0xE280AF)
+      || (c == 0xE2819F)
+      || (c == 0xE38080)
+      ;
+}
+typedef unsigned char utf8_t;
+
+#define isunicode(c) (((c)&0xc0)==0xc0)
+
+int utf8_decode(const char *str,int *i) {
+    const utf8_t *s = (const utf8_t *)str; // Use unsigned chars
+    int u = *s,l = 1;
+    if(isunicode(u)) {
+        int a = (u&0x20)? ((u&0x10)? ((u&0x08)? ((u&0x04)? 6 : 5) : 4) : 3) : 2;
+        if(a<6 || !(u&0x02)) {
+            int b,p = 0;
+            u = ((u<<(a+1))&0xff)>>(a+1);
+            for(b=1; b<a; ++b)
+                u = (u<<6)|(s[l++]&0x3f);
+        }
+    }
+    if(i) *i += l;
+    return u;
+}
+
+int utf8_encode(char *out, uint32_t utf)
+{
+  if (utf <= 0x7F) {
+    // Plain ASCII
+    out[0] = (char) utf;
+    out[1] = 0;
+    return 1;
+  }
+  else if (utf <= 0x07FF) {
+    // 2-byte unicode
+    out[0] = (char) (((utf >> 6) & 0x1F) | 0xC0);
+    out[1] = (char) (((utf >> 0) & 0x3F) | 0x80);
+    out[2] = 0;
+    return 2;
+  }
+  else if (utf <= 0xFFFF) {
+    // 3-byte unicode
+    out[0] = (char) (((utf >> 12) & 0x0F) | 0xE0);
+    out[1] = (char) (((utf >>  6) & 0x3F) | 0x80);
+    out[2] = (char) (((utf >>  0) & 0x3F) | 0x80);
+    out[3] = 0;
+    return 3;
+  }
+  else if (utf <= 0x10FFFF) {
+    // 4-byte unicode
+    out[0] = (char) (((utf >> 18) & 0x07) | 0xF0);
+    out[1] = (char) (((utf >> 12) & 0x3F) | 0x80);
+    out[2] = (char) (((utf >>  6) & 0x3F) | 0x80);
+    out[3] = (char) (((utf >>  0) & 0x3F) | 0x80);
+    out[4] = 0;
+    return 4;
+  }
+  else { 
+    // error - use replacement character
+    out[0] = (char) 0xEF;  
+    out[1] = (char) 0xBF;
+    out[2] = (char) 0xBD;
+    out[3] = 0;
+    return 0;
+  }
+}
+
 const char *nfkd(const char *input) {
-    UErrorCode status = U_ZERO_ERROR;
-    const UNormalizer2 *nfkd = unorm2_getNFKDInstance(&status);
-    if (U_FAILURE(status)) {
-        fprintf(stderr, "Error getting NFKD instance: %s\n", u_errorName(status));
-        return NULL;
-    }
+//     UErrorCode status = U_ZERO_ERROR;
+//     const UNormalizer2 *nfkd = unorm2_getNFKDInstance(&status);
+//     if (U_FAILURE(status)) {
+//         fprintf(stderr, "Error getting NFKD instance: %s\n", u_errorName(status));
+//         return NULL;
+//     }
 
-    UChar *input_u = calloc(strlen(input) + 1, sizeof(UChar));
-    if (input_u == NULL) {
-        fprintf(stderr, "Error allocating memory for input UChar\n");
-        return NULL;
-    }
-    u_strFromUTF8(input_u, strlen(input) + 1, NULL, input, strlen(input), &status);
-    if (U_FAILURE(status)) {
-        fprintf(stderr, "Error converting input to UChar: %s\n", u_errorName(status));
-        free(input_u);
-        return NULL;
-    }
+//     UChar *input_u = calloc(strlen(input) + 1, sizeof(UChar));
+//     if (input_u == NULL) {
+//         fprintf(stderr, "Error allocating memory for input UChar\n");
+//         return NULL;
+//     }
+//     u_strFromUTF8(input_u, strlen(input) + 1, NULL, input, strlen(input), &status);
+//     if (U_FAILURE(status)) {
+//         fprintf(stderr, "Error converting input to UChar: %s\n", u_errorName(status));
+//         free(input_u);
+//         return NULL;
+//     }
 
-    int32_t normalized_length = unorm2_normalize(nfkd, input_u, -1, NULL, 0, &status);
-    if (status != U_BUFFER_OVERFLOW_ERROR) {
-        fprintf(stderr, "Error getting length of normalized UChar: %s\n", u_errorName(status));
-        free(input_u);
-        return NULL;
-    }
-    status = U_ZERO_ERROR;
+//     int32_t normalized_length = unorm2_normalize(nfkd, input_u, -1, NULL, 0, &status);
+//     if (status != U_BUFFER_OVERFLOW_ERROR) {
+//         fprintf(stderr, "Error getting length of normalized UChar: %s\n", u_errorName(status));
+//         free(input_u);
+//         return NULL;
+//     }
+//     status = U_ZERO_ERROR;
 
-    UChar *normalized_u = calloc(normalized_length + 1, sizeof(UChar));
-    if (normalized_u == NULL) {
-        fprintf(stderr, "Error allocating memory for normalized UChar\n");
-        free(input_u);
-        return NULL;
-    }
+//     UChar *normalized_u = calloc(normalized_length + 1, sizeof(UChar));
+//     if (normalized_u == NULL) {
+//         fprintf(stderr, "Error allocating memory for normalized UChar\n");
+//         free(input_u);
+//         return NULL;
+//     }
 
-    unorm2_normalize(nfkd, input_u, -1, normalized_u, normalized_length + 1, &status);
-    if (U_FAILURE(status)) {
-        fprintf(stderr, "Error normalizing UChar: %s\n", u_errorName(status));
-        free(input_u);
-        free(normalized_u);
-        return NULL;
-    }
-    free(input_u);
+//     unorm2_normalize(nfkd, input_u, -1, normalized_u, normalized_length + 1, &status);
+//     if (U_FAILURE(status)) {
+//         fprintf(stderr, "Error normalizing UChar: %s\n", u_errorName(status));
+//         free(input_u);
+//         free(normalized_u);
+//         return NULL;
+//     }
+//     free(input_u);
 
-    int8_t *normalized_utf8 = calloc(normalized_length * 4 + 1, sizeof(int8_t));
-    if (normalized_utf8 == NULL) {
-        fprintf(stderr, "Error allocating memory for normalized UTF-8\n");
-        free(normalized_u);
-        return NULL;
-    }
-    u_strToUTF8((char*)normalized_utf8, normalized_length * 4 + 1, NULL, normalized_u, normalized_length, &status);
-    if (U_FAILURE(status)) {
-        fprintf(stderr, "Error converting normalized UChar to UTF-8: %s\n", u_errorName(status));
-        free(normalized_u);
-        free(normalized_utf8);
-        return NULL;
-    }
+//     UChar *normalized_utf8 = calloc(normalized_length * 4 + 1, sizeof(UChar));
+//     if (normalized_utf8 == NULL) {
+//         fprintf(stderr, "Error allocating memory for normalized UTF-8\n");
+//         free(normalized_u);
+//         return NULL;
+//     }
+//     u_strToUTF8((char*)normalized_utf8, normalized_length * 4 + 1, NULL, normalized_u, normalized_length, &status);
+//     if (U_FAILURE(status)) {
+//         fprintf(stderr, "Error converting normalized UChar to UTF-8: %s\n", u_errorName(status));
+//         free(normalized_u);
+//         free(normalized_utf8);
+//         return NULL;
+//     }
 
-free(normalized_u);
+// free(normalized_u);
 
-return (const char *)normalized_utf8;
+// return (const char *)normalized_utf8;
+  u8chr_t encoding = 0;
+  int len = u8length(input);
+  printf("u8len: %d\n", len);
+  int i=0;
+  for (; i<len && input[i] != '\0'; i++) {
+    encoding = (encoding << 8) | input[i];
+  }
+  printf("encoding: %d\n", encoding);
+  printf("chisvalid: %d\n", u8chrisvalid(encoding));
+  errno = 0;
+  if (len == 0 || !u8chrisvalid(encoding)) {
+    encoding = input[0];
+    len = 1;
+    errno = EINVAL;
+  }
+  printf("ret: %d\n", encoding ? len : 0);
+  printf("errno: %d\n", errno);
+  printf("errno: %s\n", strerror(errno));
+  uint32_t dec = u8decode(encoding);
+  printf("dec: %u\n", dec);
+  u8chr_t enc = u8encode(dec);
+  printf("enc: %d\n", enc);
+  printf("isblank: %d\n", u8chrisblank(enc));
+  printf("mbslen: %ld\n", wcslen(input));
+  printf("mbslen: %d\n", strlen(input));
+  int l, z;
+  char* output = dogecoin_char_vla(strlen(input) + 1);
+  char* out ;
+  if (len > 1) {
+    for(i=0; i<strlen(input) && input[i]!='\0'; ) {
+      if(!isunicode(input[i])) i++;
+      else {
+          l = 0;
+          z = utf8_decode(&input[i],&l);
+          printf("z: %d\n", z);
+          char* codepoint = dogecoin_char_vla(l);
+          sprintf(codepoint, "0x%04X", z);
+          // int o = utf8_encode(output, (uint32_t)codepoint);
+          printf("output: %s\n", codepoint);
+
+
+          uint32_t h;
+          sscanf(codepoint, "%x", &h);
+
+          printf("h: %d\n", h);
+          int o = utf8_encode(&out, h);
+          printf("o: %d\n", o);
+          printf("o: %s\n", &out);
+          printf("o: %d\n", u8length(&out));
+          sprintf(&output[i], "%s", &out);
+          printf("output: %s\n", output);
+          printf("output: %zu\n", strlen(output));
+          printf("Unicode value at %d is U+%04X and it\'s %d bytes.\n",i,z,l);
+          i += l;
+      }
+    }
+    return output;
+  }
+  return input;
 }
 
 // passphrase must be at most 256 characters otherwise it would be truncated
