e65bfbca0bb39ed6ec3f84d82d1ebe49598a1162
Ed Tubbs <ectubbs@gmail.com>
2022-12-28 12:27:22 -0800

    bip44: initial commit of bip44 and test

--- test/unittester.c
+++ test/unittester.c
@@ -40,6 +40,7 @@ extern void test_aes();
 extern void test_base58();
 extern void test_bip32();
 extern void test_bip39();
+extern void test_bip44();
 extern void test_block_header();
 extern void test_buffer();
 extern void test_cstr();
@@ -92,6 +93,7 @@ int main()
     u_run_test(test_base58);
     u_run_test(test_bip32);
     u_run_test(test_bip39);
+    u_run_test(test_bip44);
     u_run_test(test_block_header);
     u_run_test(test_buffer);
     u_run_test(test_cstr);
