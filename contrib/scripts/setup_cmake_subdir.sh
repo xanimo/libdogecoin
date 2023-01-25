22d10687b75f24144fdc6a1002ae8d8aedd8e981
bluezr <bluezr@dogecoin.com>
2023-01-24 19:03:17 -0800

    build: run msvc config commands via CMakeLists.txt
    -revert setup_cmake_subdir.sh changes to support linux/unix only.

--- contrib/scripts/setup_cmake_subdir.sh
+++ contrib/scripts/setup_cmake_subdir.sh
@@ -18,34 +18,20 @@ has_param() {
     return 1
 }
 
-case $(uname | tr '[:upper:]' '[:lower:]') in
-  linux*)
-    export OS_NAME=linux
-    ;;
-  darwin*)
-    export OS_NAME=osx
-    ;;
-  msys*)
-    export OS_NAME=windows
-    ;;
-  *)
-    export OS_NAME=notset
-    ;;
-esac
-
 if has_param '--path' "$@"; then
     # check if build dir exists
-    if ! test -d $2//build; then
-        mkdir -p $2//build
+    if ! test -d $2/build; then
+        mkdir -p $2/build
     fi
-    if [ $OS_NAME == "windows" ]; then
-        config='-DEVENT__LIBRARY_TYPE=STATIC -DEVENT__DISABLE_TESTS=ON -DEVENT__DISABLE_THREAD_SUPPORT=OFF -DEVENT__DISABLE_MM_REPLACEMENT=ON -DEVENT__DISABLE_REGRESS=ON'
-    else
-        config='-DEVENT__DISABLE_SHARED=ON -DEVENT__DISABLE_LIBEVENT_REGRESS=ON -DEVENT__DISABLE_DEPENDENCY_TRACKING=ON -DEVENT__ENABLE_OPTION_CHECKING=ON'
-    fi
-    config+=' -DEVENT__DISABLE_OPENSSL=ON -DEVENT__DISABLE_SAMPLES=ON'
-    pushd $2//build
-        cmake .. $config
+    pushd $2/build
+        cmake \
+        -DEVENT_DISABLE_SHARED=ON \
+        -DEVENT__DISABLE_OPENSSL=ON \
+        -DEVENT__DISABLE_LIBEVENT_REGRESS=ON \
+        -DEVENT__DISABLE_SAMPLES=ON \
+        -DEVENT__DISABLE_DEPENDENCY_TRACKING=ON \
+        -DEVENT__ENABLE_OPTION_CHECKING=ON \
+        ../.
         cmake --build .
     popd
 fi
