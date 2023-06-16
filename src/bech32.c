2b55000c7423253aba7d9bf06e234cd02b553c84
Dakoda Greaves <dakoda@xanimo.net>
2023-06-16 12:26:31 -0700

    bech32: disable -Wunused-function warning for covert_bits

--- src/bech32.c
+++ src/bech32.c
@@ -137,6 +137,8 @@ int bech32_decode(char* hrp, uint8_t *data, size_t *data_len, const char *input)
     return chk == 1;
 }
 
+DISABLE_WARNING_PUSH
+DISABLE_WARNING(-Wunused-function)
 static int convert_bits(uint8_t* out, size_t* outlen, int outbits, const uint8_t* in, size_t inlen, int inbits, int pad) {
     uint32_t val = 0;
     int bits = 0;
@@ -158,3 +160,4 @@ static int convert_bits(uint8_t* out, size_t* outlen, int outbits, const uint8_t
     }
     return 1;
 }
+DISABLE_WARNING_POP
